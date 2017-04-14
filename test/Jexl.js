b0fa539304e9f78057f3a97f2db92ba3cb8a4faa
Chris911 <christophe.naud.dulude@gmail.com>
2017-04-14 13:34:26 -0400

    Tabs to spaces

--- test/Jexl.js
+++ test/Jexl.js
@@ -4,135 +4,135 @@
  */
 
 var chai = require('chai'),
-	chaiAsPromised = require('chai-as-promised'),
-	should = require('chai').should(),
-	sinon = require('sinon'),
-	Jexl = require('../lib/Jexl'),
-	Parser = require('../lib/parser/Parser');
+  chaiAsPromised = require('chai-as-promised'),
+  should = require('chai').should(),
+  sinon = require('sinon'),
+  Jexl = require('../lib/Jexl'),
+  Parser = require('../lib/parser/Parser');
 
 chai.use(chaiAsPromised);
 
 var inst;
 
 describe('Jexl', function() {
-	beforeEach(function() {
-		inst = new Jexl.Jexl();
-	});
-	it('should resolve Promise on success', function() {
-		return inst.eval('2+2').should.become(4);
-	});
-	it('should reject Promise on error', function() {
-		return inst.eval('2++2').should.reject;
-	});
-	it('should call callback with success result', function(done) {
-		inst.eval('2+2', function(err, res) {
-			res.should.equal(4);
-			done(err);
-		});
-	});
-	it('should call callback with error result', function(done) {
-		inst.eval('2++2', function(err, res) {
-			should.exist(err);
-			should.not.exist(res);
-			done();
-		});
-	});
-	it('should allow transforms to be defined', function() {
-		inst.addTransform('toCase', function(val, args) {
-			if (args.case === 'upper')
-				return val.toUpperCase();
-			return val.toLowerCase();
-		});
-		return inst.eval('"hello"|toCase({case:"upper"})')
-			.should.become('HELLO');
-	});
-	it('should allow transforms to be retrieved', function() {
-		inst.addTransform('ret2', function() { return 2; });
-		var t = inst.getTransform('ret2');
-		should.exist(t);
-		t().should.equal(2);
-	});
-	it('should allow transforms to be set in batch', function() {
-		inst.addTransforms({
-			add1: function(val) { return val + 1; },
-			add2: function(val) { return val + 2; }
-		});
-		return inst.eval('2|add1|add2').should.become(5);
-	});
-	it('should pass context', function() {
-		return inst.eval('foo', {foo: 'bar'}).should.become('bar');
-	});
-	it('should allow binaryOps to be defined', function() {
-		inst.addBinaryOp('_=', 20, function(left, right) {
-			return left.toLowerCase() === right.toLowerCase();
-		});
-		return inst.eval('"FoO" _= "fOo"').should.become(true);
-	});
-	it('should observe weight on binaryOps', function() {
-		inst.addBinaryOp('**', 0, function(left, right) {
-			return left * 2 + right * 2;
-		});
-		inst.addBinaryOp('***', 1000, function(left, right) {
-			return left * 2 + right * 2;
-		});
-		return Promise.all([
-			inst.eval('1 + 2 ** 3 + 4'),
-			inst.eval('1 + 2 *** 3 + 4')
-		]).should.become([20, 15]);
-	});
-	it('should allow unaryOps to be defined', function() {
-		inst.addUnaryOp('~', function(right) {
-			return Math.floor(right);
-		});
-		return inst.eval('~5.7 + 5').should.become(10);
-	});
-	it('should allow binaryOps to be removed', function() {
-		inst.removeOp('+');
-		return inst.eval('1+2').should.reject;
-	});
-	it('should allow unaryOps to be removed', function() {
-		inst.removeOp('!');
-		return inst.eval('!true').should.reject;
-	});
+  beforeEach(function() {
+    inst = new Jexl.Jexl();
+  });
+  it('should resolve Promise on success', function() {
+    return inst.eval('2+2').should.become(4);
+  });
+  it('should reject Promise on error', function() {
+    return inst.eval('2++2').should.reject;
+  });
+  it('should call callback with success result', function(done) {
+    inst.eval('2+2', function(err, res) {
+      res.should.equal(4);
+      done(err);
+    });
+  });
+  it('should call callback with error result', function(done) {
+    inst.eval('2++2', function(err, res) {
+      should.exist(err);
+      should.not.exist(res);
+      done();
+    });
+  });
+  it('should allow transforms to be defined', function() {
+    inst.addTransform('toCase', function(val, args) {
+      if (args.case === 'upper')
+        return val.toUpperCase();
+      return val.toLowerCase();
+    });
+    return inst.eval('"hello"|toCase({case:"upper"})')
+      .should.become('HELLO');
+  });
+  it('should allow transforms to be retrieved', function() {
+    inst.addTransform('ret2', function() { return 2; });
+    var t = inst.getTransform('ret2');
+    should.exist(t);
+    t().should.equal(2);
+  });
+  it('should allow transforms to be set in batch', function() {
+    inst.addTransforms({
+      add1: function(val) { return val + 1; },
+      add2: function(val) { return val + 2; }
+    });
+    return inst.eval('2|add1|add2').should.become(5);
+  });
+  it('should pass context', function() {
+    return inst.eval('foo', {foo: 'bar'}).should.become('bar');
+  });
+  it('should allow binaryOps to be defined', function() {
+    inst.addBinaryOp('_=', 20, function(left, right) {
+      return left.toLowerCase() === right.toLowerCase();
+    });
+    return inst.eval('"FoO" _= "fOo"').should.become(true);
+  });
+  it('should observe weight on binaryOps', function() {
+    inst.addBinaryOp('**', 0, function(left, right) {
+      return left * 2 + right * 2;
+    });
+    inst.addBinaryOp('***', 1000, function(left, right) {
+      return left * 2 + right * 2;
+    });
+    return Promise.all([
+      inst.eval('1 + 2 ** 3 + 4'),
+      inst.eval('1 + 2 *** 3 + 4')
+    ]).should.become([20, 15]);
+  });
+  it('should allow unaryOps to be defined', function() {
+    inst.addUnaryOp('~', function(right) {
+      return Math.floor(right);
+    });
+    return inst.eval('~5.7 + 5').should.become(10);
+  });
+  it('should allow binaryOps to be removed', function() {
+    inst.removeOp('+');
+    return inst.eval('1+2').should.reject;
+  });
+  it('should allow unaryOps to be removed', function() {
+    inst.removeOp('!');
+    return inst.eval('!true').should.reject;
+  });
 
-	describe('parser cache', function() {
-		var static_inst = new Jexl.Jexl();
+  describe('parser cache', function() {
+    var static_inst = new Jexl.Jexl();
 
-		before(function() {
-			sinon.spy(Parser.prototype, 'complete');
-			sinon.spy(static_inst, '_clearParserCache');
-		});
-		beforeEach(function() {
-			Parser.prototype.complete.reset();
-			static_inst._clearParserCache.reset();
-		});
-		after(function() {
-			Parser.prototype.complete.restore();
-			static_inst._clearParserCache.restore();
-		});
+    before(function() {
+      sinon.spy(Parser.prototype, 'complete');
+      sinon.spy(static_inst, '_clearParserCache');
+    });
+    beforeEach(function() {
+      Parser.prototype.complete.reset();
+      static_inst._clearParserCache.reset();
+    });
+    after(function() {
+      Parser.prototype.complete.restore();
+      static_inst._clearParserCache.restore();
+    });
 
-		it('should call parser.complete on the first run', function() {
-			return static_inst.eval('8+8')
-				.then(function(res) {
-					res.should.equal(16);
-					Parser.prototype.complete.callCount.should.equal(1);
-				});
-		});
+    it('should call parser.complete on the first run', function() {
+      return static_inst.eval('8+8')
+        .then(function(res) {
+          res.should.equal(16);
+          Parser.prototype.complete.callCount.should.equal(1);
+        });
+    });
 
-		it('should not call parser.complete on the second run', function() {
-			return static_inst.eval('8+8')
-				.then(function(res) {
-					res.should.equal(16);
-					Parser.prototype.complete.callCount.should.equal(0);
-				});
-		});
+    it('should not call parser.complete on the second run', function() {
+      return static_inst.eval('8+8')
+        .then(function(res) {
+          res.should.equal(16);
+          Parser.prototype.complete.callCount.should.equal(0);
+        });
+    });
 
-		it('should clear the parser cache when adding a new op', function() {
-			static_inst.addBinaryOp('**', 0, function(left, right) {
-				return left * 2 + right * 2;
-			});
+    it('should clear the parser cache when adding a new op', function() {
+      static_inst.addBinaryOp('**', 0, function(left, right) {
+        return left * 2 + right * 2;
+      });
 
-			static_inst._clearParserCache.callCount.should.equal(1);
-		});
-	});
+      static_inst._clearParserCache.callCount.should.equal(1);
+    });
+  });
 });
--- test/Lexer.js
+++ test/Lexer.js
@@ -4,154 +4,154 @@
  */
 
 var should = require('chai').should(),
-	Lexer = require('../lib/Lexer'),
-	grammar = require('../lib/grammar').elements;
+  Lexer = require('../lib/Lexer'),
+  grammar = require('../lib/grammar').elements;
 
 var inst;
 
 describe('Lexer', function() {
-	beforeEach(function() {
-		inst = new Lexer(grammar);
-	});
-	describe('Elements', function() {
-		it("should count a string as one element", function() {
-			var str = '"foo"',
-				elems = inst.getElements(str);
-			elems.should.have.length(1);
-			elems[0].should.equal(str);
-		});
-		it("should support single-quote strings", function() {
-			var str = "'foo'",
-				elems = inst.getElements(str);
-			elems.should.have.length(1);
-			elems[0].should.equal(str);
-		});
-		it("should support escaping double-quotes", function() {
-			var str = '"f\\"oo"',
-				elems = inst.getElements(str);
-			elems.should.have.length(1);
-			elems[0].should.equal(str);
-		});
-		it("should support escaping single-quotes", function() {
-			var str = "'f\\'oo'",
-				elems = inst.getElements(str);
-			elems.should.have.length(1);
-			elems[0].should.equal(str);
-		});
-		it("should count an identifier as one element", function() {
-			var str = "alpha12345",
-				elems = inst.getElements(str);
-			elems.should.deep.equal([str]);
-		});
-		it("should not split grammar elements out of transforms", function() {
-			var str = "inString",
-				elems = inst.getElements(str);
-			elems.should.deep.equal([str]);
-		});
-	});
-	describe('Tokens', function() {
-		it("should unquote string elements", function() {
-			var tokens = inst.getTokens(['"foo \\"bar\\\\"']);
-			tokens.should.deep.equal([{
-				type: 'literal',
-				value: 'foo "bar\\',
-				raw: '"foo \\"bar\\\\"'
-			}]);
-		});
-		it("should recognize booleans", function() {
-			var tokens = inst.getTokens(['true', 'false']);
-			tokens.should.deep.equal([
-				{
-					type: 'literal',
-					value: true,
-					raw: 'true'
-				},
-				{
-					type: 'literal',
-					value: false,
-					raw: 'false'
-				}
-			]);
-		});
-		it("should recognize numerics", function() {
-			var tokens = inst.getTokens(['-7.6', '20']);
-			tokens.should.deep.equal([
-				{
-					type: 'literal',
-					value: -7.6,
-					raw: '-7.6'
-				},
-				{
-					type: 'literal',
-					value: 20,
-					raw: '20'
-				}
-			]);
-		});
-		it("should recognize binary operators", function() {
-			var tokens = inst.getTokens(['+']);
-			tokens.should.deep.equal([{
-				type: 'binaryOp',
-				value: '+',
-				raw: '+'
-			}]);
-		});
-		it("should recognize unary operators", function() {
-			var tokens = inst.getTokens(['!']);
-			tokens.should.deep.equal([{
-				type: 'unaryOp',
-				value: '!',
-				raw: '!'
-			}]);
-		});
-		it("should recognize control characters", function() {
-			var tokens = inst.getTokens(['(']);
-			tokens.should.deep.equal([{
-				type: 'openParen',
-				value: '(',
-				raw: '('
-			}]);
-		});
-		it("should recognize identifiers", function() {
-			var tokens = inst.getTokens(['_foo9_bar']);
-			tokens.should.deep.equal([{
-				type: 'identifier',
-				value: '_foo9_bar',
-				raw: '_foo9_bar'
-			}]);
-		});
-		it("should throw on invalid token", function() {
-			var fn = inst.getTokens.bind(Lexer, ['9foo']);
-			fn.should.throw();
-		});
-	});
-	it("should tokenize a full expression", function() {
-		var tokens = inst.tokenize('6+x -  -17.55*y<= !foo.bar["baz\\"foz"]');
-		tokens.should.deep.equal([
-			{type: 'literal', value: 6, raw: '6'},
-			{type: 'binaryOp', value: '+', raw: '+'},
-			{type: 'identifier', value: 'x', raw: 'x '},
-			{type: 'binaryOp', value: '-', raw: '-  '},
-			{type: 'literal', value: -17.55, raw: '-17.55'},
-			{type: 'binaryOp', value: '*', raw: '*'},
-			{type: 'identifier', value: 'y', raw: 'y'},
-			{type: 'binaryOp', value: '<=', raw: '<= '},
-			{type: 'unaryOp', value: '!', raw: '!'},
-			{type: 'identifier', value: 'foo', raw: 'foo'},
-			{type: 'dot', value: '.', raw: '.'},
-			{type: 'identifier', value: 'bar', raw: 'bar'},
-			{type: 'openBracket', value: '[', raw: '['},
-			{type: 'literal', value: 'baz"foz', raw: '"baz\\"foz"'},
-			{type: 'closeBracket', value: ']', raw: ']'}
-		]);
-	});
-	it("should consider minus to be negative appropriately", function() {
-		inst.tokenize('-1?-2:-3').should.deep.equal([
-			{type: 'literal', value: -1, raw: '-1'},
-			{type: 'question', value: '?', raw: '?'},
-			{type: 'literal', value: -2, raw: '-2'},
-			{type: 'colon', value: ':', raw: ':'},
-			{type: 'literal', value: -3, raw: '-3'}
-		]);
-	});
+  beforeEach(function() {
+    inst = new Lexer(grammar);
+  });
+  describe('Elements', function() {
+    it("should count a string as one element", function() {
+      var str = '"foo"',
+        elems = inst.getElements(str);
+      elems.should.have.length(1);
+      elems[0].should.equal(str);
+    });
+    it("should support single-quote strings", function() {
+      var str = "'foo'",
+        elems = inst.getElements(str);
+      elems.should.have.length(1);
+      elems[0].should.equal(str);
+    });
+    it("should support escaping double-quotes", function() {
+      var str = '"f\\"oo"',
+        elems = inst.getElements(str);
+      elems.should.have.length(1);
+      elems[0].should.equal(str);
+    });
+    it("should support escaping single-quotes", function() {
+      var str = "'f\\'oo'",
+        elems = inst.getElements(str);
+      elems.should.have.length(1);
+      elems[0].should.equal(str);
+    });
+    it("should count an identifier as one element", function() {
+      var str = "alpha12345",
+        elems = inst.getElements(str);
+      elems.should.deep.equal([str]);
+    });
+    it("should not split grammar elements out of transforms", function() {
+      var str = "inString",
+        elems = inst.getElements(str);
+      elems.should.deep.equal([str]);
+    });
+  });
+  describe('Tokens', function() {
+    it("should unquote string elements", function() {
+      var tokens = inst.getTokens(['"foo \\"bar\\\\"']);
+      tokens.should.deep.equal([{
+        type: 'literal',
+        value: 'foo "bar\\',
+        raw: '"foo \\"bar\\\\"'
+      }]);
+    });
+    it("should recognize booleans", function() {
+      var tokens = inst.getTokens(['true', 'false']);
+      tokens.should.deep.equal([
+        {
+          type: 'literal',
+          value: true,
+          raw: 'true'
+        },
+        {
+          type: 'literal',
+          value: false,
+          raw: 'false'
+        }
+      ]);
+    });
+    it("should recognize numerics", function() {
+      var tokens = inst.getTokens(['-7.6', '20']);
+      tokens.should.deep.equal([
+        {
+          type: 'literal',
+          value: -7.6,
+          raw: '-7.6'
+        },
+        {
+          type: 'literal',
+          value: 20,
+          raw: '20'
+        }
+      ]);
+    });
+    it("should recognize binary operators", function() {
+      var tokens = inst.getTokens(['+']);
+      tokens.should.deep.equal([{
+        type: 'binaryOp',
+        value: '+',
+        raw: '+'
+      }]);
+    });
+    it("should recognize unary operators", function() {
+      var tokens = inst.getTokens(['!']);
+      tokens.should.deep.equal([{
+        type: 'unaryOp',
+        value: '!',
+        raw: '!'
+      }]);
+    });
+    it("should recognize control characters", function() {
+      var tokens = inst.getTokens(['(']);
+      tokens.should.deep.equal([{
+        type: 'openParen',
+        value: '(',
+        raw: '('
+      }]);
+    });
+    it("should recognize identifiers", function() {
+      var tokens = inst.getTokens(['_foo9_bar']);
+      tokens.should.deep.equal([{
+        type: 'identifier',
+        value: '_foo9_bar',
+        raw: '_foo9_bar'
+      }]);
+    });
+    it("should throw on invalid token", function() {
+      var fn = inst.getTokens.bind(Lexer, ['9foo']);
+      fn.should.throw();
+    });
+  });
+  it("should tokenize a full expression", function() {
+    var tokens = inst.tokenize('6+x -  -17.55*y<= !foo.bar["baz\\"foz"]');
+    tokens.should.deep.equal([
+      {type: 'literal', value: 6, raw: '6'},
+      {type: 'binaryOp', value: '+', raw: '+'},
+      {type: 'identifier', value: 'x', raw: 'x '},
+      {type: 'binaryOp', value: '-', raw: '-  '},
+      {type: 'literal', value: -17.55, raw: '-17.55'},
+      {type: 'binaryOp', value: '*', raw: '*'},
+      {type: 'identifier', value: 'y', raw: 'y'},
+      {type: 'binaryOp', value: '<=', raw: '<= '},
+      {type: 'unaryOp', value: '!', raw: '!'},
+      {type: 'identifier', value: 'foo', raw: 'foo'},
+      {type: 'dot', value: '.', raw: '.'},
+      {type: 'identifier', value: 'bar', raw: 'bar'},
+      {type: 'openBracket', value: '[', raw: '['},
+      {type: 'literal', value: 'baz"foz', raw: '"baz\\"foz"'},
+      {type: 'closeBracket', value: ']', raw: ']'}
+    ]);
+  });
+  it("should consider minus to be negative appropriately", function() {
+    inst.tokenize('-1?-2:-3').should.deep.equal([
+      {type: 'literal', value: -1, raw: '-1'},
+      {type: 'question', value: '?', raw: '?'},
+      {type: 'literal', value: -2, raw: '-2'},
+      {type: 'colon', value: ':', raw: ':'},
+      {type: 'literal', value: -3, raw: '-3'}
+    ]);
+  });
 });
