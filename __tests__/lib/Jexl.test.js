cc9b7f124db7422c52906ee349df116a3e46559a
Tobias Hettler <tobias.hettler@gmx.de>
2019-01-21 15:58:01 +0100

    Add sync behavior Fixes #40

--- __tests__/lib/Jexl.test.js
+++ __tests__/lib/Jexl.test.js
@@ -10,8 +10,8 @@ describe('Jexl', () => {
   beforeEach(() => {
     inst = new Jexl.Jexl()
   })
-  it('resolves Promise on success', async () => {
-    await expect(inst.eval('2+2')).resolves.toBe(4)
+  it('resolves Promise on success', () => {
+    expect(inst.eval('2+2')).resolves.toBe(4)
   })
   it('rejects Promise on error', async () => {
     await expect(inst.eval('2++2')).rejects.toThrow(/unexpected/)
@@ -51,6 +51,69 @@ describe('Jexl', () => {
   it('allows unaryOps to be defined', async () => {
     inst.addUnaryOp('~', (right) => Math.floor(right))
     await expect(inst.eval('~5.7 + 5')).resolves.toBe(10)
+  })
+  it('allows binaryOps to be removed', async () => {
+    inst.removeOp('+')
+    await expect(inst.eval('1+2')).rejects.toThrow(/invalid/i)
+  })
+  it('allows unaryOps to be removed', async () => {
+    inst.removeOp('!')
+    await expect(inst.eval('!true')).rejects.toThrow(/invalid/i)
+  })
+})
+
+describe('Sync Jexl', () => {
+  beforeEach(() => {
+    inst = new Jexl.Jexl()
+  })
+
+  it('sync eval', () => {
+    expect(inst.syncEval('true == true')).toBe(true)
+  })
+  it('resolves Promise on success', () => {
+    expect(inst.syncEval('2+2')).toBe(4)
+  })
+  it('rejects Promise on error', () => {
+    const t = () => {
+      inst.syncEval('2++2')
+    }
+    expect(t).toThrow(/unexpected/)
+  })
+  it('allows transforms to be defined', () => {
+    inst.addTransform('toCase', (val, args) => args.case === 'upper' ? val.toUpperCase() : val.toLowerCase())
+    expect(inst.syncEval('"hello"|toCase({case:"upper"})')).toBe('HELLO')
+  })
+  it('allows transforms to be retrieved', () => {
+    inst.addTransform('ret2', () => 2)
+    const t = inst.getTransform('ret2')
+    expect(t).toBeDefined()
+    expect(t()).toBe(2)
+  })
+  it('allows transforms to be set in batch', () => {
+    inst.addTransforms({
+      add1: (val) => val + 1,
+      add2: (val) => val + 2
+    })
+    expect(inst.syncEval('2|add1|add2')).toBe(5)
+  })
+  it('passes context', async () => {
+    await expect(inst.syncEval('foo', { foo: 'bar' })).toBe('bar')
+  })
+  it('allows binaryOps to be defined', () => {
+    inst.addBinaryOp('_=', 20, (left, right) => left.toLowerCase() === right.toLowerCase())
+    expect(inst.syncEval('"FoO" _= "fOo"')).toBe(true)
+  })
+  it('observes weight on binaryOps', () => {
+    inst.addBinaryOp('**', 0, (left, right) => left * 2 + right * 2)
+    inst.addBinaryOp('***', 1000, (left, right) => left * 2 + right * 2)
+    expect([
+      inst.syncEval('1 + 2 ** 3 + 4'),
+      inst.syncEval('1 + 2 *** 3 + 4')
+    ]).toEqual([20, 15])
+  })
+  it('allows unaryOps to be defined', () => {
+    inst.addUnaryOp('~', (right) => Math.floor(right))
+    expect(inst.syncEval('~5.7 + 5')).toBe(10)
   })
   it('allows binaryOps to be removed', async () => {
     inst.removeOp('+')
--- __tests__/lib/evaluator/Evaluator.test.js
+++ __tests__/lib/evaluator/Evaluator.test.js
@@ -157,3 +157,140 @@ describe('Evaluator', () => {
     return expect(e.eval(toTree('"".length'))).resolves.toBe(0)
   })
 })
+
+describe('Sync Evaluator', () => {
+  it('evaluates an arithmetic expression', async () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('(2 + 3) * 4'))).toBe(20)
+  })
+  it('evaluates a string concat', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('"Hello" + (4+4) + "Wo\\"rld"'))).toBe('Hello8Wo"rld')
+  })
+  it('evaluates a true comparison expression', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('2 > 1'))).toBe(true)
+  })
+  it('evaluates a false comparison expression', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('2 <= 1'))).toBe(false)
+  })
+  it('evaluates a complex expression', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('"foo" && 6 >= 6 && 0 + 1 && true'))).toBe(true)
+  })
+  it('evaluates an identifier chain', () => {
+    const context = { foo: { baz: { bar: 'tek' } } }
+    const e = new Evaluator(grammar, null, context)
+    return expect(e.syncEval(toTree('foo.baz.bar'))).toBe(context.foo.baz.bar)
+  })
+  it('applys transforms', () => {
+    const context = { foo: 10 }
+    const half = val => val / 2
+    const e = new Evaluator(grammar, { half: half }, context)
+    return expect(e.syncEval(toTree('foo|half + 3'))).toBe(8)
+  })
+  it('filters arrays', () => {
+    const context = {
+      foo: {
+        bar: [
+          { tek: 'hello' },
+          { tek: 'baz' },
+          { tok: 'baz' }
+        ]
+      }
+    }
+    const e = new Evaluator(grammar, null, context)
+    return expect(e.syncEval(toTree('foo.bar[.tek == "baz"]'))).toEqual([{ tek: 'baz' }])
+  })
+  it('assumes array index 0 when traversing', () => {
+    const context = {
+      foo: {
+        bar: [
+          { tek: { hello: 'world' } },
+          { tek: { hello: 'universe' } }
+        ]
+      }
+    }
+    const e = new Evaluator(grammar, null, context)
+    return expect(e.syncEval(toTree('foo.bar.tek.hello'))).toBe('world')
+  })
+  it('makes array elements addressable by index', () => {
+    const context = {
+      foo: {
+        bar: [
+          { tek: 'tok' },
+          { tek: 'baz' },
+          { tek: 'foz' }
+        ]
+      }
+    }
+    const e = new Evaluator(grammar, null, context)
+    return expect(e.syncEval(toTree('foo.bar[1].tek'))).toBe('baz')
+  })
+  it('allows filters to select object properties', () => {
+    const context = { foo: { baz: { bar: 'tek' } } }
+    const e = new Evaluator(grammar, null, context)
+    return expect(e.syncEval(toTree('foo["ba" + "z"].bar'))).toBe(context.foo.baz.bar)
+  })
+  it('throws when transform does not exist', () => {
+    const e = new Evaluator(grammar)
+    const t = () => {
+      e.syncEval(toTree('"hello"|world'))
+    }
+    return expect(t).toThrow(Error)
+  })
+  it('applys the DivFloor operator', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('7 // 2'))).toBe(3)
+  })
+  it('evaluates an object literal', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('{foo: {bar: "tek"}}'))).toEqual({ foo: { bar: 'tek' } })
+  })
+  it('evaluates an empty object literal', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('{}'))).toEqual({})
+  })
+  it('evaluates a transform with multiple args', () => {
+    const e = new Evaluator(grammar, {
+      concat: (val, a1, a2, a3) => val + ': ' + a1 + a2 + a3
+    })
+    return expect(e.syncEval(toTree('"foo"|concat("baz", "bar", "tek")'))).toBe('foo: bazbartek')
+  })
+  it('evaluates dot notation for object literals', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('{foo: "bar"}.foo'))).toBe('bar')
+  })
+  it('allows access to literal properties', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('"foo".length'))).toBe(3)
+  })
+  it('evaluates array literals', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('["foo", 1+2]'))).toEqual(['foo', 3])
+  })
+  it('applys the "in" operator to strings', () => {
+    const e = new Evaluator(grammar)
+    expect(e.syncEval(toTree('"bar" in "foobartek"'))).toBe(true)
+    expect(e.syncEval(toTree('"baz" in "foobartek"'))).toBe(false)
+  })
+  it('applys the "in" operator to arrays', () => {
+    const e = new Evaluator(grammar)
+    expect(e.syncEval(toTree('"bar" in ["foo","bar","tek"]'))).toBe(true)
+    expect(e.syncEval(toTree('"baz" in ["foo","bar","tek"]'))).toBe(false)
+  })
+  it('evaluates a conditional expression', () => {
+    const e = new Evaluator(grammar)
+    expect(e.syncEval(toTree('"foo" ? 1 : 2'))).toBe(1)
+    expect(e.syncEval(toTree('"" ? 1 : 2'))).toBe(2)
+  })
+  it('allows missing consequent in ternary', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('"foo" ?: "bar"'))).toBe('foo')
+  })
+  it('does not treat falsey properties as undefined', () => {
+    const e = new Evaluator(grammar)
+    return expect(e.syncEval(toTree('"".length'))).toBe(0)
+  })
+})
